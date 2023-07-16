82ca86fd265a7ae2d825cd5d4b753f529dd4f389
professorleandrocesar <professor@leandrocesar.co>
2023-07-16 19:39:59 -0300

    updt

--- components/LogoHome.vue
+++ components/LogoHome.vue
@@ -10,7 +10,7 @@ import LCLogo from '@/components/LCLogo.vue'
                 <h1>Espírita Top</h1>
             </span>
             <span class='logoAnimationSevenTwo'>
-                <h3></h3>
+                <h3>Você sendo um espírita Top</h3>
             </span>
         </div>
     </header>
@@ -49,7 +49,7 @@ h3 {
     color: var(--color-text);
   transition: all .3s linear;
   cursor: pointer;
-  margin:-15px 0 50px 0;
+  margin: -10px 0 50px 0;
   text-align: center;
     font-size: 1rem;
 }
